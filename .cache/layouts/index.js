6782cce7fd50defc1cc889ac05667f976ee521e3
Matthew McClure <m@mmcc.io>
2018-03-15 10:20:26 -0700

    delete the cache so we can ignore it

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/mmcc/Projects/demuxed/demuxed.com/src/layouts/index.jsx"
-  import data from "/Users/mmcc/Projects/demuxed/demuxed.com/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
