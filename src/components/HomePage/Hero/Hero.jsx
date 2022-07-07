d34aa7d69f8cabf29889803295cd1ff8f9112345
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2022-07-07 14:17:40 +0100

    Add 2022 hero banner and link

--- src/components/HomePage/Hero/Hero.jsx
+++ src/components/HomePage/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import Container from '../../common/Container';
 import HeroBanner from './HeroBanner';
 import HeroEventCard from './HeroEventCard';
-import bannerImage from '../../../images/demuxed-2021-hero.png';
+import bannerImage from '../../../images/demuxed-2022-hero.png';
 import { brandBlue, large, medLarge, medium } from '../../../styles/variables';
 import { fontSize, media } from '../../../styles/mixins';
 
@@ -88,7 +88,7 @@ const Hero = ({ events }) => (
           </LeftContent>
         ) : null}
         <RightContent>
-          <StyledBanner url="https://2021.demuxed.com" imageUrl={bannerImage} />
+          <StyledBanner url="https://2022.demuxed.com" imageUrl={bannerImage} />
         </RightContent>
       </ContentWrapper>
     </Container>
